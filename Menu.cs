747175cc0e67fcf69fd9fe409e056ef367148d0d
Leonardo de Souza Rodrigues <leosouza1392@gmail.com>
2022-08-30 23:42:30 -0300

    feat: Implemented DrawScreen function

--- Menu.cs
+++ Menu.cs
@@ -8,8 +8,35 @@ namespace HtmlEditor
         public static void Show()
         {
             Console.Clear();
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            DrawScreen();
+
+        }
+
+        public static void DrawScreen()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+            Console.Write("+");
+            Console.Write("\n");
+
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                    Console.Write(" ");
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+            Console.Write("+");
+            Console.Write("\n");
 
         }
     }
